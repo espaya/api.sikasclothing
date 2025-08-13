38c32fb99206340ce2a551670ab76849500214e4
espaya <beehivelive8@gmail.com>
2025-08-13 03:00:14 +0000

    Added delete wishlist

--- app/Http/Controllers/Api/Customer/CustomerWishlistController.php
+++ app/Http/Controllers/Api/Customer/CustomerWishlistController.php
@@ -13,6 +13,7 @@ class CustomerWishlistController extends Controller
 {
     public function index()
     {
+
         try {
             if (!Auth::check()) {
                 return response()->json(['message' => 'Unauthorized'], 401);
@@ -20,17 +21,40 @@ class CustomerWishlistController extends Controller
 
             $user = Auth::user();
 
-            Log::info('Auth ID: ' . $user);
-
             $wishlists = $user->wishlist()
-                ->with('product')
+                ->with(['product.categories'])
                 ->orderBy('id', 'DESC')
                 ->paginate(10);
+
 
             return response()->json($wishlists);
         } catch (\Exception $ex) {
             Log::error($ex->getMessage());
             return response()->json(['message' => 'An error occurred. Please try again later.'], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            if (!Auth::check()) {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+
+            $user = Auth::user();
+
+            $wishlist = $user->wishlist()->where('product_id', $id)->first();
+
+            if (!$wishlist) {
+                return response()->json(['message' => 'Product not found in your wishlist'], 404);
+            }
+
+            $wishlist->delete();
+
+            return response()->json(['message' => 'Product removed from wishlist'], 200);
+        } catch (Exception $ex) {
+            Log::error('Error removing product from wishlist: ' . $ex->getMessage() . ' on line ' . $ex->getLine());
+            return response()->json(['message' => 'Error removing product from wishlist'], 500);
         }
     }
 }
